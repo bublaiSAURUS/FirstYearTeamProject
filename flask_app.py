91272d9505653e7a7dc932d9eaa00c0e99e0c1c5
g89496oe <oliver.edmonds@student.manchester.ac.uk>
2023-03-19 21:24:09 +0000

    Added the cookies

--- flask_app.py
+++ flask_app.py
@@ -20,17 +20,61 @@ def linear():
     return resp
 @app.route('/linearSolve',methods = ['POST', 'GET'])
 def linearSolve():
+    resp = make_response(render_template('ResultsPage.html'))
+    variables = request.cookies.get('variables').replace(",","@")
+    constants = request.cookies.get('constants').replace(",","@")
+    variableList = variables.split("&")
+    constantsList = constants.split("@")
+    for i in range(len(variableList)):
+        variableList[i] = variableList[i].split("@")
+        for j in range(len(variableList[i])):
+            variableList[i][j] = float(variableList[i][j])
+        constantsList[i] = float(constantsList[i])
+    l = LinearSystem(variableList,constantsList)
+    resp.set_cookie('variables', variables)
+    resp.set_cookie('constants', constants)
+    solutions = ','.join(str(x) for x in list(l.solve_linear_system()))
+    resp.set_cookie('solution',solutions.replace(",","@"))
+    return resp
+@app.route('/nonLinear',methods = ['POST','GET'])
+def nonLinear():
+    resp = make_response(render_template('nonLinear.html'))
+    resp.set_cookie('variables', 'test')
+    resp.set_cookie('constants', 'test2')
+    return resp
+@app.route('/nonLinearSolve',methods = ['POST', 'GET'])
+def nonLinearSolve():
     variables = request.cookies.get('variables')
     constants = request.cookies.get('constants')
     print(variables)
-    variableList = variables.split("-")
+    variableList = variables.split("&")
     constantsList = constants.split(",")
     for i in range(len(variableList)):
         variableList[i] = variableList[i].split(",")
         for j in range(len(variableList[i])):
             variableList[i][j] = float(variableList[i][j])
         constantsList[i] = float(constantsList[i])
-    l = LinearSystem(variableList,constantsList)
-    return list(l.solve_linear_system())
+    return variableList
+@app.route('/ODE',methods = ['POST','GET'])
+def ODE():
+    resp = make_response(render_template('ODE.html'))
+    resp.set_cookie('variables', 'test')
+    resp.set_cookie('constants', 'test2')
+    return resp
+@app.route('/ODESolve',methods = ['POST', 'GET'])
+def ODESolve():
+    coefficients = request.cookies.get('coefficients')
+    initialConditions = request.cookies.get('initialConditions')
+    initialConditionsList = initialConditions.split('&')
+    initialConditionsDict = {}
+    for initialCondition in initialConditionsList:
+        values = initialCondition.split(",")
+        for i in range(len(values)):
+            values[i] = float(values[i])
+        initialConditionsDict[values[0]] = values[1:]
+    coefficientsList = coefficients.split(",")
+    for i in range(len(coefficientsList)):
+        coefficientsList[i] = float(coefficientsList[i])
+    return initialConditionsDict
 if __name__ == '__main__':
     app.run(debug=True)
